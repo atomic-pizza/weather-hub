3d03d71da5434077089115cfa2bffcfed54312fa
atomic-pizza <brandonsmith85@gmail.com>
2026-06-09 02:32:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,32 @@ function weatherDescription(code){
         65:"🌧 Heavy Rain",
         95:"⛈ Thunderstorm"
     };
+    function formatHour(timeString){
+
+    const date = new Date(timeString);
+
+    return date.toLocaleTimeString(
+        [],
+        {
+            hour:'numeric',
+            hour12:true
+        }
+    );
+
+}
+    function formatDay(dateString,index){
+
+    if(index===0) return "Today";
+
+    if(index===1) return "Tomorrow";
+
+    return new Date(dateString)
+        .toLocaleDateString(
+            [],
+            { weekday:'long' }
+        );
+
+}
 
     return map[code] || "Unknown";
 }
@@ -119,36 +145,68 @@ async function openForecast(index){
         </div>
     `;
 
-    let hourlyHtml='';
+    let hourlyHtml='<div class="hourly-scroll">';
 
-    for(let i=0;i<24;i++){
+for(let i=0;i<24;i++){
 
-        hourlyHtml+=`
-            <div class="hour-row">
-                ${data.hourly.time[i].split('T')[1]}
-                -
-                ${Math.round(data.hourly.temperature_2m[i])}°F
+    hourlyHtml+=`
+        <div class="hour-card">
+
+            <div>
+                ${formatHour(data.hourly.time[i])}
             </div>
-        `;
-    }
 
-    document.getElementById('hourlyForecast').innerHTML=
-    hourlyHtml;
-
-    let dailyHtml='';
-
-    for(let i=0;i<data.daily.time.length;i++){
-
-        dailyHtml+=`
-            <div class="day-row">
-                ${data.daily.time[i]}
-                :
-                ${Math.round(data.daily.temperature_2m_max[i])}°
-                /
-                ${Math.round(data.daily.temperature_2m_min[i])}°
+            <div class="temp">
+                ${Math.round(
+                    data.hourly.temperature_2m[i]
+                )}°
             </div>
-        `;
-    }
+
+        </div>
+    `;
+}
+
+hourlyHtml+='</div>';
+
+document.getElementById(
+    'hourlyForecast'
+).innerHTML=hourlyHtml;
+
+    let dailyHtml='<div class="daily-scroll">';
+
+for(let i=0;i<data.daily.time.length;i++){
+
+    dailyHtml+=`
+        <div class="day-card">
+
+            <div>
+                ${formatDay(
+                    data.daily.time[i],
+                    i
+                )}
+            </div>
+
+            <div class="temp">
+                ${Math.round(
+                    data.daily.temperature_2m_max[i]
+                )}°
+            </div>
+
+            <div>
+                ${Math.round(
+                    data.daily.temperature_2m_min[i]
+                )}°
+            </div>
+
+        </div>
+    `;
+}
+
+dailyHtml+='</div>';
+
+document.getElementById(
+    'dailyForecast'
+).innerHTML=dailyHtml;
 
     document.getElementById('dailyForecast').innerHTML=
     dailyHtml;
